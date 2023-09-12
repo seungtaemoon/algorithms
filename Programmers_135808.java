e27e52046d410ed6f533d1a21e548436fb1dcf15
문승태 <conanmoon@gmail.com>
2023-09-12 18:07:09 +0900

    Update Programmers_135808.java

--- Programmers_135808.java
+++ Programmers_135808.java
@@ -7,25 +7,24 @@ import java.util.Collections;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        // 1. 배열을 정렬
-        Arrays.sort(score);
-        // 2. 새로운 배열을 생성할 가변형 배열 선언
+        Integer res[] = Arrays.stream(score).boxed().toArray(Integer[]::new);
+        Arrays.sort(res, Collections.reverseOrder());
         ArrayList<Integer> arr = new ArrayList<>();
-        // 3. 기존 배열을 순회
-        for(int i = score.length-1; i > 0; i--){
-            // 4. 배열의 크기가 m-1보다 큰 한 순회 계속하면서 score배열의 가장 끝 부분부터 가변형 배열에 추가
-            if (i >= m-1){
-                arr.add(score[i]);
-                // 5. 배열의 출력
-                System.out.println(score[i]);
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < res.length; i++){
+            if (res.length % m < m){
+                if (i < m){
+                    arr.add(res[i]);
+                    System.out.println(arr.get(i));
+                }
+                else{
+                    break;
+                }
             }
-            // 6. 배열의 크기가 m-1보다 작은 경우 반복문 탈출
             else{
-                break;
+                
             }
         }
-        // 7. 배열의 최소 값과 배열의 크기를 곱하기
-        int res = Collections.min(arr) * arr.size();
-        return res;
+        return arr;
     }
 }
