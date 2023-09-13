40c8392502169ba4dee771d53cc911f70a464ab8
문승태 <conanmoon@gmail.com>
2023-09-13 18:44:59 +0900

    Update Programmers_135808.java

--- Programmers_135808.java
+++ Programmers_135808.java
@@ -1,30 +1,16 @@
 // 프로그래머스 132267번 문제 - 콜라 문제
-// 상태: 미해결
+// 상태: 해결(참조: https://isshosng.tistory.com/174?category=1033272)
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        Integer res[] = Arrays.stream(score).boxed().toArray(Integer[]::new);
-        Arrays.sort(res, Collections.reverseOrder());
-        ArrayList<Integer> arr = new ArrayList<>();
-        ArrayList<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < res.length; i++){
-            if (res.length % m < m){
-                if (i < m){
-                    arr.add(res[i]);
-                    System.out.println(arr.get(i));
-                }
-                else{
-                    break;
-                }
-            }
-            else{
-                
-            }
+        Arrays.sort(score);
+        int answer = 0;
+        for(int i = score.length; i >= m; i -= m){
+            answer += m * score[i - m];
         }
-        return arr;
+        return answer;
     }
 }
